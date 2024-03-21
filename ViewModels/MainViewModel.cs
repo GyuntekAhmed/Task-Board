d2251c038830dd3b1a886d88a8ddb8287977f1f5
gyuntekahmed <gyuntekahmed@abv.bg>
2024-03-21 18:55:10 +0200

    Add Main view model and podify UI

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace Task_Board.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty] 
+        string text;
+
+        [ICommand]
+        void Add()
+        {
+            if (string.IsNullOrEmpty(Text))
+                return;
+
+            Items.Add(Text);
+            // Add our item
+            Text = string.Empty;
+        }
+
+        [ICommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+            {
+                Items.Remove(s);
+            }
+        }
+    }
+}
